92df9f2436e821b6a26d0a9d4cb4252351f9228f
LukaBabunadze <luka.babunadze.1@iliauni.edu.ge>
2025-03-05 22:39:54 +0400

    Initial Commit

---
+++ PersonalRecipe/PersonalRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalRecipeApp.swift
+//  PersonalRecipe
+//
+//  Created by Luka Babunadze on 05.03.25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
